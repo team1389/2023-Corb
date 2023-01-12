c3098d95de7574cad056cf12b853d66db803fcee
Ria Gulati <riagulati111@gmail.com>
2023-01-12 16:36:39 -0500

    stuff

--- src/main/java/frc/subsystems/SwerveWheel.java
+++ src/main/java/frc/subsystems/SwerveWheel.java
@@ -6,4 +6,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveWheel extends SubsystemBase {
     private CANSparkMax rotationMotor, driveMotor;
+    public SwerveWheel(CANSparkMax rotationMotor, CANSparkMax driveMotor){
+        driveMotor = new CANSparkMax(deviceId, type);
+    }
+    
 }
