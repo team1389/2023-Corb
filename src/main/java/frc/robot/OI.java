903721f21fdb142f329fdde9f1185663fcd6e410
Max Ohm <maxdemohm@gmail.com>
2023-01-31 18:18:05 -0500

    outtake command

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.commands.AutoBalance;
+import frc.commands.RunIntake;
+import frc.commands.RunOuttake;
 import frc.commands.TeleOpDrive;
 import frc.commands.UpdatePosition;
 //import frc.commands.Test;
@@ -18,16 +20,22 @@ import frc.robot.RobotMap.AutoConstants;
 //import frc.autos.TestAuto;
 import frc.commands.UpdatePosition;
 import frc.subsystems.Drivetrain;
+import frc.subsystems.Intake;
 import frc.subsystems.Vision;
 
 public class OI {
 
     public final Drivetrain drivetrain = new Drivetrain();
     public final Vision vision = new Vision();
+    public final Intake intake = new Intake();
 
     private XboxController driveController;
     private Trigger driveRightBumper;
     private Trigger driveAButton;
+
+    private XboxController manipController;
+    private Trigger manipAButton;
+    private Trigger manipBButton;
 
     public OI() {
         initControllers();
@@ -46,8 +54,10 @@ public class OI {
         // Press right bumper -> zero gyro heading
         driveRightBumper.onTrue(new InstantCommand(()->drivetrain.zeroHeading()));
 
-
         vision.setDefaultCommand(new UpdatePosition(drivetrain, vision));
+        
+        manipAButton.onTrue(new RunIntake(intake));
+        manipBButton.onTrue(new RunOuttake(intake));
     }
 
     /**
@@ -57,6 +67,10 @@ public class OI {
         driveController = new XboxController(0);
         driveRightBumper = new JoystickButton(driveController, XboxController.Button.kRightBumper.value);
         driveAButton = new JoystickButton(driveController, XboxController.Button.kA.value);
+
+        manipController = new XboxController(1);
+        manipAButton = new JoystickButton(manipController, XboxController.Button.kA.value);
+        manipBButton = new JoystickButton(manipController, XboxController.Button.kB.value);
     }
 
     // Return autocommand
@@ -67,7 +81,6 @@ public class OI {
         // );
 
         // Command trajCommand = drivetrain.followTrajectoryCommand(trajectory, true);
-
         // return trajCommand;
         return new AutoBalance(drivetrain);
     }
