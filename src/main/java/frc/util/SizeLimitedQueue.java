907a83f91baa282410c0588628f5db9f3c42b3f0
frogtd <31412003+frogtd@users.noreply.github.com>
2023-03-09 17:31:03 -0500

    detect slowdown

--- src/main/java/frc/util/SizeLimitedQueue.java
+++ src/main/java/frc/util/SizeLimitedQueue.java
@@ -11,7 +11,7 @@ public class SizeLimitedQueue {
         this.size = size;
     }
 
-    public void addElement(double number) {
+    public void add(double number) {
         queue.add(number);
         if (queue.size() > size) {
             queue.remove();
@@ -20,21 +20,36 @@ public class SizeLimitedQueue {
 
     public double getSum() {
         double sum = 0;
-        for(double i : queue) {
+        for (double i : queue) {
             sum += i;
         }
 
         return sum;
     }
+
     public double length() {
         return size;
     }
 
     public double getAverage() {
         double sum = 0;
-        for(Double num: queue){
-            sum+=num;
+        for (Double num : queue) {
+            sum += num;
         }
-        return sum/queue.size();
+        return sum / queue.size();
+    }
+
+    public double getAverageDerivative() {
+        double sum = 0;
+        double[] array = new double[queue.size()];
+        int i = 0;
+        for (Double num : queue) {
+            array[i] = num;
+            i++;
+        }
+        for (int j = 0; j < array.length - 1; j++) {
+            sum += (array[j + 1] - array[j]) / 1;
+        }
+        return sum / (array.length);
     }
 }
